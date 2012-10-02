a6a28072543653280508fe439eea7208906be69a
xuecheng <acheng.meee@yahoo.cn>
2012-10-03 01:34:01 +0800

    cheng

--- plugins/category_generator.rb
+++ plugins/category_generator.rb
@@ -20,19 +20,6 @@
 #                          'Category: ').
 
 module Jekyll
-	
-	class CategoryListTag < Liquid::Tag
-		def render(context)
-			html = ""
-			categories = context.registers[:site].categories.keys
-			dir = context.registers[:site].config['category_dir']
-			categories.sort!.map do |category|
-				posts_in_category = context.registers[:site].categories[category].size
-				url =  dir+"/"+category.gsub(/_|\P{Word}/, '-').gsub(/-{2,}/, '-').downcase
-				html << "<li class='category'><a href=' /#{url}/'>#{category} (#{posts_in_category})</a></li>\n"
-			end
-			html
-		end
 
   # The CategoryIndex class creates a single category page for the specified category.
   class CategoryIndex < Page
